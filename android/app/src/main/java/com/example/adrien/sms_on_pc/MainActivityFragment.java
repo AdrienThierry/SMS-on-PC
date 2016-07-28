ed8aa900fc39adc228c60142e120406681bda6aa
Adrien Thierry <adrienthierry2@gmail.com>
2016-07-28 19:10:57 +0200

    Android : show wifi ip address

--- android/app/src/main/java/com/example/adrien/sms_on_pc/MainActivityFragment.java
+++ android/app/src/main/java/com/example/adrien/sms_on_pc/MainActivityFragment.java
@@ -1,15 +1,20 @@
 package com.example.adrien.sms_on_pc;
 
+import android.net.wifi.WifiManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    TextView ipAddress;
 
     public MainActivityFragment() {
     }
@@ -17,6 +22,18 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ipAddress = (TextView)view.findViewById(R.id.ipAddress);
+
+        // Get WiFi IP address
+        WifiManager wm = (WifiManager) getActivity().getSystemService(getContext().WIFI_SERVICE);
+        int ip = wm.getConnectionInfo().getIpAddress();
+        String ipString = String.format("%d.%d.%d.%d", (ip & 0xff),(ip >> 8 & 0xff),(ip >> 16 & 0xff),(ip >> 24 & 0xff));
+
+        // Show IP address
+        ipAddress.setText(ipString);
+
+        return view;
     }
 }
