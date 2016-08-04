919161b4df9b217176fa1a0333bdad6d3917e0dc
Adrien Thierry <adrienthierry2@gmail.com>
2016-08-04 17:38:31 +0200

    [Server] Added function to create unique device IDs

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,7 @@
 var constants = {
 	config_file_name: "conf.json",
-	config_route: "conf"
+	config_route: "conf",
+	last_id_file_name: "last_id"
 };
 
 module.exports = constants;
---
+++ server/id_generator.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var constants = require("./constants.js");
+
+// Check if last_id file exists
+function get_new_id() {
+	try {
+		fs.accessSync(constants.last_id_file_name, fs.R_OK | fs.W_OK)
+		//code to action if file exists
+		var buf = fs.readFileSync(constants.last_id_file_name);
+		var last_id = parseInt(buf);
+		var new_id = last_id + 1;
+		fs.writeFileSync(constants.last_id_file_name, new_id.toString());
+
+		return new_id;
+
+	} catch(e) {
+		//code to action if file does not exist
+		fs.writeFileSync(constants.last_id_file_name, "0");
+
+		return 0;
+	}
+}
+
+module.exports.get_new_id = get_new_id;
+
+
